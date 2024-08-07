ee2ff2929524fdc900c1eaa9ff552d43f3d71755
Mariana <mariana.perez.consolaro@gmail.com>
2024-08-07 17:16:18 -0300

    Javascript
    How to use Javascript

--- course-react/src/components/FirstComponent.js
+++ course-react/src/components/FirstComponent.js
@@ -2,7 +2,10 @@
 function FirstComponent() {
   return (
     <div className="firstcomponent">
+        {/* Comentário no JSX */}
       <p>Primeiro Componente</p>
+      {2+2}
+      {console.log("JavaScript")}
     </div>
   );
 }
